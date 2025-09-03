8672728eec22c976d65a594e65b7e34cda1fc1f6
M.Hamza <152792061+hamzasand@users.noreply.github.com>
2025-09-03 21:06:17 +0500

    Update salary_cal.py

--- salary_cal.py
+++ salary_cal.py
@@ -1,7 +1,7 @@
 ### Use of List Comprehension ###
 working_hours =[int(x) for x in input(" Enter your working hours per day seperated by spaces ").split()] 
 wage = int(input("Enter you hourly wega"))
-total = sum(working_hours)
+total = sum(working_hours) # Sum Function builtin
 salary = total * wage # Logic to calculate salary
 
 print(" Salary is ", salary)
